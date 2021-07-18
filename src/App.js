d94904deeb96020488bdb9da6c84ffc71e40eff9
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-07-18 17:12:08 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlinePhone,
+  AiOutlineEdit,
+  AiOutlineCustomerService,
+} from "react-icons/ai";
+import { RiMapPinRangeLine } from "react-icons/ri";
+import { FaAngellist } from "react-icons/fa";
 
 function App() {
   return (
     <div className="Menu-bar">
       <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Services</li>
-        <li>Clients</li>
-        <li>Investers</li>
-        <li>Pricing</li>
-        <li>Training</li>
-        <li>Contacts</li>
+        <li>
+          <AiOutlineHome />
+          Home
+        </li>
+        <li>
+          <AiOutlineUser />
+          About Us
+          <div className="sub-menu-1">
+            <ul>
+              <li>Mission</li>
+              <li>Vision</li>
+              <li>Team</li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <AiOutlineCustomerService />
+          Services
+        </li>
+        <li>
+          <AiOutlineUser />
+          Clients
+        </li>
+        <li>
+          <FaAngellist />
+          Investers
+          <div className="sub-menu-1">
+            <ul>
+              <li>Mission</li>
+              <li className='hover-me'>
+                Vision
+                <div className="sub-menu-2">
+                  <ul>
+                    <li>Deo</li>
+                    <li>Teo</li>
+                    <li>Seo</li>
+                  </ul>
+                </div>
+              </li>
+              <li>Team</li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <RiMapPinRangeLine />
+          Pricing
+        </li>
+        <li>
+          <AiOutlineEdit />
+          Training
+        </li>
+        <li>
+          <AiOutlinePhone />
+          Contacts
+        </li>
       </ul>
     </div>
   );
