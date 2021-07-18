00aa6d4a106eaae00e8d07b5e4dac74f317ce044
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-07-18 16:14:21 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>njbvc </h1>
+    <div className="Menu-bar">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Clients</li>
+        <li>Investers</li>
+        <li>Pricing</li>
+        <li>Training</li>
+        <li>Contacts</li>
+      </ul>
     </div>
   );
 }
